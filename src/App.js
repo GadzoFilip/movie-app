64eb674b8a963df202316eab20807cecaf46f7af
Filip Gadzo <filip.gadzo@fastlane.net>
2022-04-19 16:05:26 +0200

    Minor fixes and changes

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   }, [searchValue]);
 
   return (
-    <div className="">
+    <div>
       <Navigation
         searchValue={searchValue}
         setSearchValue={setSearchValue}
